ad2bca1238c35ae4d40d061df67907da6eba21df
Nishantvidhuri <nishantvidhuri098&@gmail.com>
2025-02-09 22:57:45 +0530

    myprojects section bug fixed

---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import FirstSectionHome from './FirstSectionHome'
+import Projectsuggestions from './Projectsuggestions'
+
+function MyProjects() {
+  return (
+    <div>
+        <Navbar/>
+        <FirstSectionHome/>
+        <Projectsuggestions/>
+    </div>
+  )
+}
+
+export default MyProjects
