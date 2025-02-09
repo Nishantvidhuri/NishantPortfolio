c804ed02af97297a1b7749d5df912d70f6eb717c
Nishantvidhuri <nishantvidhuri098&@gmail.com>
2025-02-09 16:52:09 +0530

    section updated

--- src/components/FirstSectionHome.jsx
+++ src/components/FirstSectionHome.jsx
@@ -57,8 +57,25 @@ function FirstSectionHome() {
               href={projects[randomIndex].livelink}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-white text-black font-semibold px-3 sm:px-4 py-2 sm:py-3 rounded transition"
+              className="flex items-center  gap-2 bg-white text-black font-semibold  px-3 sm:px-4 py-2 sm:py-3 rounded transition"
             >
+              <div className="w-4 sm:w-5 h-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  role="img"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="18"
+                  aria-hidden="true"
+                  className="text-black"
+                >
+                  <path
+                    d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
               <span className="text-sm text-black sm:text-md">Live Demo</span>
             </a>
 
@@ -68,6 +85,25 @@ function FirstSectionHome() {
               rel="noopener noreferrer"
               className="flex items-center gap-2 bg-[#716A63] text-white font-semibold w-28 sm:w-32 md:w-40 justify-center h-10 sm:h-12 opacity-60 rounded transition hover:opacity-50"
             >
+              <div className="w-6 sm:w-7 h-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  role="img"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="22"
+                  aria-hidden="true"
+                  className="text-white"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
               <span className="text-sm sm:text-lg">GitHub</span>
             </a>
           </div>
@@ -123,8 +159,25 @@ function FirstSectionHome() {
               href={projects[randomIndex].livelink}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-white text-black font-semibold w-40 justify-center px-3 py-2 rounded transition"
+              className="flex items-center gap-2 bg-white text-black font-semibold w-32 justify-center px-3 py-2 rounded transition"
             >
+              <div className="w-4 sm:w-5 h-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  role="img"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="18"
+                  aria-hidden="true"
+                  className="text-black"
+                >
+                  <path
+                    d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
               <span className="text-sm text-black">Live Demo</span>
             </a>
 
@@ -132,8 +185,28 @@ function FirstSectionHome() {
               href={projects[randomIndex].githublink}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-[#716A66] text-white font-semibold w-44 justify-center h-10 opacity-60 rounded transition hover:opacity-50"
+              className="flex items-center gap-2 bg-[#716A66] text-white font-semibold w-36 justify-center h-10 opacity-60 rounded transition hover:opacity-50"
             >
+              <div className="w-6 sm:w-7 h-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  role="img"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="22"
+                  aria-hidden="true"
+                  className="text-white"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
               <span className="text-sm">GitHub</span>
             </a>
           </div>
